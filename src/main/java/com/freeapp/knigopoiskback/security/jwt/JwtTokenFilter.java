006078067876b51fa3d31084c8bc2568974bb0a2
andrey.gorevoy <andrei.haravy@helmes.ee>
2022-07-01 13:33:37 +0300

    Fix problem with cors origin from external server

--- src/main/java/com/freeapp/knigopoiskback/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/freeapp/knigopoiskback/security/jwt/JwtTokenFilter.java
@@ -9,30 +9,40 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
  * @author Andrei Gorevoi
  */
 public class JwtTokenFilter extends GenericFilterBean {
-        private final JwtTokenProvider jwtTokenProvider;
+  private final JwtTokenProvider jwtTokenProvider;
 
-        public JwtTokenFilter(final JwtTokenProvider jwtTokenProvider) {
-                this.jwtTokenProvider = jwtTokenProvider;
-        }
+  public JwtTokenFilter(final JwtTokenProvider jwtTokenProvider) {
+    this.jwtTokenProvider = jwtTokenProvider;
+  }
 
-        @Override
-        public void doFilter(
-            ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws IOException, ServletException {
-                String token = jwtTokenProvider.resolveToken((HttpServletRequest) servletRequest);
-                if (token != null && jwtTokenProvider.validateToken(token)) {
-                        Authentication authentication = jwtTokenProvider.getAuthentication(token);
-                        if (authentication != null) {
-                                SecurityContextHolder.getContext().setAuthentication(authentication);
-                        }
-                }
-                filterChain.doFilter(servletRequest, servletResponse);
-
-        }
+  @Override
+  public void doFilter(
+      ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+      throws IOException, ServletException {
+    HttpServletResponse response = (HttpServletResponse) servletResponse;
+    HttpServletRequest request = (HttpServletRequest) servletRequest;
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept, X-Auth-Token, authorization");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+    response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    String token = jwtTokenProvider.resolveToken(request);
+    if (token != null && jwtTokenProvider.validateToken(token)) {
+      Authentication authentication = jwtTokenProvider.getAuthentication(token);
+      if (authentication != null) {
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+      }
+    }
+    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+      response.setStatus(HttpServletResponse.SC_OK);
+    } else {
+      filterChain.doFilter(servletRequest, servletResponse);
+    }
+  }
 }
